da110fcbbb521a1b5827ec90517898cf9b42f105
Nils Geilen <7262115+nilsgeilen@users.noreply.github.com>
2023-07-26 23:11:54 +0200

    Added further charts

--- dice.py
+++ dice.py
@@ -1,7 +1,8 @@
+import statistics
 from collections import Counter
 from functools import reduce
 from itertools import combinations
-from typing import Dict, Set, List
+from typing import Dict, Set, List, Callable, Sequence
 
 import matplotlib.pyplot as plt
 import os
@@ -22,7 +23,9 @@ def chart(title, data: Dict[int, float], xlabel=None):
 def multi_chart(title, data: Dict[str, Dict[int, float]], bar=False, ylabel='chance [%]', xlabel=None):
     plt.figure(figsize=(7, 4))
     all_xvals = [key for series in data.values() for key in series.keys()]
-    plt.xticks(range(min(all_xvals), max(all_xvals) + 1))
+    xmin = min(all_xvals)
+    xmax = max(all_xvals)
+    plt.xticks(range(xmin, xmax + 1))
 
     if bar:
         width = 0.8 / len(data)
@@ -30,6 +33,9 @@ def multi_chart(title, data: Dict[str, Dict[int, float]], bar=False, ylabel='cha
             xvals = [key + (i + 0.5) * width - 0.4 for key in data[label].keys()]
             plt.bar(xvals, data[label].values(), label=label, width=width)
     else:
+        ymax = max(max(curve.values()) for curve in data.values())
+        for y in range(0, int(ymax) + 20, 20):
+            plt.hlines(y=y, xmin=xmin, xmax=xmax, linewidth=0.5, colors='black')
         for label in data:
             xvals = sorted(data[label])
             yvals = [data[label][key] for key in xvals]
@@ -49,7 +55,7 @@ def combine(n):
     return set(combinations(dice, n))
 
 
-def percentage(f, combos: Set[int]) -> Dict[int, float]:
+def probability_distribution(f: Callable[[Sequence[int]], int], combos: Set[Set[int]]) -> Dict[int, float]:
     val_cnt = Counter(map(f, combos))
     sigma = sum(val_cnt.values())
     return {key: val / sigma * 100.0 for key, val in val_cnt.items()}
@@ -93,6 +99,13 @@ def hits(threshold: int):
     return lambda l: sum(1 if x >= threshold else 0 for x in l)
 
 
+def hit(threshold: int, combo: set[int]) -> int:
+    for die in combo:
+        if die >= threshold:
+            return 1
+    return 0
+
+
 def explosive_die(threshold: int, computational_depth: int):
     sequences = combine(computational_depth)
 
@@ -105,42 +118,55 @@ def explosive_die(threshold: int, computational_depth: int):
                 break
         return i
 
-    return percentage(explode, sequences)
+    return probability_distribution(explode, sequences)
 
 
-chart('Probability of 1 d6', percentage(sum, combine(1)), xlabel='Σ(1 d6)')
-chart('Probability of the Sum of 2 d6', percentage(sum, combine(2)), xlabel='Σ(2 d6)')
-chart('Probability of the Sum of 3 d6', percentage(sum, combine(3)), xlabel='Σ(3 d6)')
-chart('Probability of the Sum of 4 d6', percentage(sum, combine(4)), xlabel='Σ(4 d6)')
+combos = {n: combine(n) for n in range(1, 7)}
+
+multi_chart('Probability to Score at Least 1 Hit', {
+    f'{threshold}+': {n: sum(hit(threshold, combo) for combo in combos[n]) / len(combos[n]) * 100.0 for n in
+                      range(1, 7)} for
+    threshold in range(2, 7)}, xlabel='number of dice rolled')
+
+chart('Probability of 1 d6', probability_distribution(sum, combine(1)), xlabel='Σ(1 d6)')
+chart('Probability of the Sum of 2 d6', probability_distribution(sum, combine(2)), xlabel='Σ(2 d6)')
+chart('Probability of the Sum of 3 d6', probability_distribution(sum, combine(3)), xlabel='Σ(3 d6)')
+chart('Probability of the Sum of 4 d6', probability_distribution(sum, combine(4)), xlabel='Σ(4 d6)')
 
 multi_chart('Cumulative Probability of the Sum of n d6',
-            {f'Σ({i} d6)': cumulative(percentage(sum, combine(i))) for i in range(2, 5)},
+            {f'Σ({i} d6)': cumulative(probability_distribution(sum, combine(i))) for i in range(2, 5)},
             ylabel='cumulative chance [%]')
 
-multi_chart('Probability of the Maximum of n d6', {f'max({i} d6)': percentage(max, combine(i)) for i in range(2, 5)},
+multi_chart('Probability of the Maximum of n d6',
+            {f'max({i} d6)': probability_distribution(max, combine(i)) for i in range(2, 5)},
             bar=True)
 
 multi_chart('Probability of the Minimum of 2 d6', {
-    'min(2 d6)': percentage(min, combine(2)),
-    'min(2 d6 w/o  doubles)': percentage(min, {x for x in combine(2) if list(x)[0] != list(x)[1]})
+    'min(2 d6)': probability_distribution(min, combine(2)),
+    'min(2 d6 w/o  doubles)': probability_distribution(min, {x for x in combine(2) if list(x)[0] != list(x)[1]})
 }, bar=True)
 
-chart('Probability of the Difference between 2 d6', percentage(delta, combine(2)), xlabel='Δ(2 d6)')
-chart('Π(2 d6)', percentage(lambda l: l[0] * l[1], combine(2)))
+chart('Probability of the Difference between 2 d6', probability_distribution(delta, combine(2)), xlabel='Δ(2 d6)')
+chart('Π(2 d6)', probability_distribution(lambda l: l[0] * l[1], combine(2)))
+
+multi_chart('Probability of the Median of n d6',
+            {f'{n} d6': probability_distribution(statistics.median, combine(n)) for n in (3, 5, 7)}, xlabel='median',
+            bar=True)
 
 for i in range(1, 7):
     multi_chart(f'Probability of Hits with {i} d6',
                 {f'{threshold}+, μ = {round(expected_value(percentage), 1)}': percentage for threshold, percentage in
-                 [(k, percentage(hits(k), combine(i))) for k in range(2, 7)]}, xlabel='hits')
+                 [(k, probability_distribution(hits(k), combine(i))) for k in range(2, 7)]}, xlabel='hits')
 
     multi_chart(f'Cumulative Probability of Hits with {i} d6',
-                {f'{threshold}+': cumulative(percentage(hits(threshold), combine(i))) for threshold in range(2, 7)},
+                {f'{threshold}+': cumulative(probability_distribution(hits(threshold), combine(i))) for threshold in
+                 range(2, 7)},
                 ylabel='cumulative chance [%]', xlabel='hits')
 
-    for threshold in range(2, 7):
-        c5d6e = explosive_die(threshold, 7)
+for threshold in range(2, 7):
+    c5d6e = explosive_die(threshold, 7)
     for i in range(1, 7):
-        p1 = percentage(hits(threshold), combine(i))
+        p1 = probability_distribution(hits(threshold), combine(i))
     mu1 = round(expected_value(p1), 1)
     p2 = combine_percentages([c5d6e] * i)
     mu2 = round(expected_value(p2), 1)
@@ -151,4 +177,8 @@ for i in range(1, 7):
 for i in range(2, 7):
     multi_chart(f'Probability of Hits with n d6 ({i}+)',
                 {f'{n} d6, μ = {round(expected_value(percentage), 1)}': percentage for n, percentage in
-                 [(n, percentage(hits(i), combine(n))) for n in range(1, 7)]}, xlabel='hits')
+                 [(n, probability_distribution(hits(i), combine(n))) for n in range(1, 7)]}, xlabel='hits')
+
+    multi_chart(f'Cumulative Probability of Hits with n d6 ({i}+)',
+                {f'{n} d6': cumulative(probability_distribution(hits(i), combine(n))) for n in range(1, 7)},
+                ylabel='cumulative chance [%]', xlabel='hits')
